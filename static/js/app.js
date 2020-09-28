bb620c03c3919b04a01ebef5a81d8c65f6f1b3ff
mmead <mmeadx@gmail.com>
2020-09-27 21:48:25 -0500

    Dropdown working with states populated

--- static/js/app.js
+++ static/js/app.js
@@ -2,9 +2,35 @@ const url = "https://api.covidtracking.com";
 
 // Set up variables to take in States
 var usaOverall = "/v1/us/daily.json"
+var current = "/v1/states/current.json"
 var state = "mn"
 var stateDaily = `/v1/states/${state}/daily.json`
 
-var current = d3.json(url + stateDaily).then(function(data) {
-    console.log(data);
-})
+function init() {
+
+    var current = "/v1/states/current.json"
+    var select = d3.select("#selDataset");
+
+    var stateID= []
+
+    // Loops through States to get data
+    d3.json(url + current).then((state) => {
+        for (var i = 0; i < state.length; i++) {
+           
+           // STATE CODE FOR DROPDOWN 
+           statecode = state[i].state;
+           stateID.push(statecode);
+
+        }
+        console.log("States Added to Dropdown: ");
+        console.log(stateID);
+
+        stateID.forEach((state) => {
+            select.append("option").text(state).property("value", state);
+        });
+
+        var firstState = stateID[0];
+    });
+};
+
+init();
