7175413a98cc54535ac28d890cfe09bb0593df1f
mmead <mmeadx@gmail.com>
2020-10-07 16:32:58 -0500

    working on pulling data out of json

--- static/js/app.js
+++ static/js/app.js
@@ -6,11 +6,16 @@ var current = "/v1/states/current.json"
 var state = "mn"
 var stateDaily = `/v1/states/${state}/daily.json`
 
-// CHARTS FUNCITON
-function charts() {
+d3.json(url +current).then((state) => {
+    state.forEach(function(d) {
+        var numbers = d3.select("#todaysnumbers")
+            .append("h6")
+            .html(`${d.state}: ${d.death}`);
+    });
 
-};
-
+    var today = d3.select("#todaysdate")
+        .html(`${state[0].date}`);
+});
 
 
 // INITIALIZE FUNCTION
@@ -44,5 +49,23 @@ function init() {
         var firstState = stateID[0];
     });
 };
-
 init();
+
+// var myMap = L.map("map", {
+//     center: [44.97, -93.2650],
+//     zoom: 14
+//   });
+  
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+//   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//     tileSize: 512,
+//     maxZoom: 18,
+//     zoomOffset: -1,
+//     id: "mapbox/streets-v11",
+//     accessToken: API_KEY
+//   }).addTo(myMap);
+
+//   console.log(myMap);
+
